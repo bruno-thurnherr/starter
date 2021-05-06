36d388990d5bb8dd72b9e857f13020d34c0147c5
Bruno Thurnherr <bruno.thurnherr@it-wissen.ch>
2021-05-06 21:38:32 +0200

    Update Code

--- src/main/java/ch/abbts/nds/swe/swdt/starter/EmployeesController.java
+++ src/main/java/ch/abbts/nds/swe/swdt/starter/EmployeesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
-// Update
+
 // @RestController
 // @RequestMapping(path = "/employees")
 
